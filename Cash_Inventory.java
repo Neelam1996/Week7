14e728afd47690a495cae9c08d6c6ef10fd27000
Neelam1996 <36359791+Neelam1996@users.noreply.github.com>
2018-03-21 01:21:07 +0530

    Add files via upload

---
+++ Cash_Inventory.java
@@ -0,0 +1,13 @@
+package Week_7;
+
+import java.util.*;
+
+public interface Cash_Inventory {
+
+	public void initialize_CashInventory();
+
+	public void addCoin(Coin c, int no);
+
+	public void removeCoin(Coin c, int no);
+
+}
